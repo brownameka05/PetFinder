bffbef8ba1682888bbac2bc21c57e66df812c4df
ttlekich <ttlekich@gmail.com>
2018-10-23 09:58:24 -0500

    wiring up filters

--- js/filters/filtersUpdate.js
+++ js/filters/filtersUpdate.js
@@ -1,56 +1,100 @@
 // Document Selectors
-const typeSelector = document.getElementById('select-type');
-const breedSelector = document.getElementById('select-breed');
-const sizeSelector = document.getElementById('select-size');
-const sexSelector = document.getElementById('select-sex');
-const locationSelector = document.getElementById('textbox-location');
-const listViewButton = document.getElementById('filters-list');
-const mapViewButton = document.getElementById('filters-map');
+const typeSelector = document.getElementById("select-type")
+const breedSelector = document.getElementById("select-breed")
+const sizeSelector = document.getElementById("select-size")
+const sexSelector = document.getElementById("select-sex")
+const locationSelector = document.getElementById("textbox-location")
+const listViewButton = document.getElementById("filters-list")
+const mapViewButton = document.getElementById("filters-map")
+const filtersList = document.getElementsByClassName("filters-list")[0]
 
-const filtersList = document.getElementsByClassName('filters-list')[0];
-
-typeSelector.addEventListener('change', (e) => {
-  console.log(e.target);
-  // setFilters(petFilters, {})
-});
-
-const eventHandler = (filterObj, field) => {
+const updateType = () => {
   return function(data) {
     petFilters = setFilters(petFilters, {
-      field: [data, ...filterObj[field]]
-    });
-  };
-};
+      animals: [data, ...petFilters.animals]
+    })
+  }
+}
 
-$('#select-type').selectize({
-  plugins: ['remove_button'],
-  sortField: 'text',
+$("#select-type").selectize({
+  plugins: ["remove_button"],
+  sortField: "text",
   create: true,
   // passes the value as the agument
-  onItemAdd: eventHandler(petFilters, 'animals')
-});
+  onItemAdd: updateType()
+})
 
-$('#select-breed').selectize({
-  plugins: ['remove_button'],
-  sortField: 'text'
-});
+const updateBreeds = () => {
+  return function(data) {
+    petFilters = setFilters(petFilters, {
+      breeds: [data, ...petFilters.breeds]
+    })
+  }
+}
 
-$('#select-size').selectize({
-  plugins: ['remove_button'],
-  sortField: 'text'
-});
+$("#select-breed").selectize({
+  plugins: ["remove_button"],
+  sortField: "text",
+  onItemAdd: updateBreeds()
+})
 
-$('#select-sex').selectize({
-  plugins: ['remove_button'],
-  sortField: 'text'
-});
+const updateSizes = () => {
+  return function(data) {
+    petFilters = setFilters(petFilters, {
+      sizes: [data, ...petFilters.sizes]
+    })
+  }
+}
 
-$('#select-age').selectize({
-  plugins: ['remove_button'],
-  sortField: 'text'
-});
+$("#select-size").selectize({
+  plugins: ["remove_button"],
+  sortField: "text",
+  onItemAdd: updateSizes()
+})
 
-const toggleBurger = (e) => {
-  e.classList.toggle('change');
-  filtersList.classList.toggle('open');
-};
+const updateSexes = () => {
+  return function(data) {
+    petFilters = setFilters(petFilters, {
+      sexes: [data, ...petFilters.sexes]
+    })
+  }
+}
+
+$("#select-sex").selectize({
+  plugins: ["remove_button"],
+  sortField: "text",
+  onItemAdd: updateSexes()
+})
+
+const updateAges = () => {
+  return function(data) {
+    petFilters = setFilters(petFilters, {
+      ages: [data, ...petFilters.ages]
+    })
+  }
+}
+
+$("#select-age").selectize({
+  plugins: ["remove_button"],
+  sortField: "text",
+  onItemAdd: updateAges()
+})
+
+const updateLocation = () => {
+  return function(data) {
+    petFilters = setFilters(petFilters, {
+      location: data
+    })
+  }
+}
+
+$("#textbox-location").selectize({
+  plugins: ["remove_button"],
+  sortField: "text",
+  onItemAdd: updateLocation()
+})
+
+const toggleBurger = e => {
+  e.classList.toggle("change")
+  filtersList.classList.toggle("open")
+}
