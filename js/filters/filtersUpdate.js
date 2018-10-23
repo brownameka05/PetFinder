c43bedc519591d1e20ba6952820a6f4636cdbfff
Travis Lekich <ttlekich@gmail.com>
2018-10-22 22:35:05 -0500

    feat: styling for filter component

--- js/filters/filtersUpdate.js
+++ js/filters/filtersUpdate.js
@@ -1,49 +1,56 @@
 // Document Selectors
-const typeSelector = document.getElementById("select-type")
-const breedSelector = document.getElementById("select-breed")
-const sizeSelector = document.getElementById("select-size")
-const sexSelector = document.getElementById("select-sex")
-const locationSelector = document.getElementById("textbox-location")
-const listViewButton = document.getElementById("filters-list")
-const mapViewButton = document.getElementById("filters-map")
+const typeSelector = document.getElementById('select-type');
+const breedSelector = document.getElementById('select-breed');
+const sizeSelector = document.getElementById('select-size');
+const sexSelector = document.getElementById('select-sex');
+const locationSelector = document.getElementById('textbox-location');
+const listViewButton = document.getElementById('filters-list');
+const mapViewButton = document.getElementById('filters-map');
 
-typeSelector.addEventListener("change", e => {
-  console.log(e.target)
+const filtersList = document.getElementsByClassName('filters-list')[0];
+
+typeSelector.addEventListener('change', (e) => {
+  console.log(e.target);
   // setFilters(petFilters, {})
-})
+});
 
 const eventHandler = (filterObj, field) => {
   return function(data) {
     petFilters = setFilters(petFilters, {
       field: [data, ...filterObj[field]]
-    })
-  }
-}
+    });
+  };
+};
 
-$("#select-type").selectize({
-  plugins: ["remove_button"],
-  sortField: "text",
+$('#select-type').selectize({
+  plugins: ['remove_button'],
+  sortField: 'text',
   create: true,
   // passes the value as the agument
-  onItemAdd: eventHandler(petFilters, "animals")
-})
+  onItemAdd: eventHandler(petFilters, 'animals')
+});
 
-$("#select-breed").selectize({
-  plugins: ["remove_button"],
-  sortField: "text"
-})
+$('#select-breed').selectize({
+  plugins: ['remove_button'],
+  sortField: 'text'
+});
 
-$("#select-size").selectize({
-  plugins: ["remove_button"],
-  sortField: "text"
-})
+$('#select-size').selectize({
+  plugins: ['remove_button'],
+  sortField: 'text'
+});
 
-$("#select-sex").selectize({
-  plugins: ["remove_button"],
-  sortField: "text"
-})
+$('#select-sex').selectize({
+  plugins: ['remove_button'],
+  sortField: 'text'
+});
 
-$("#select-age").selectize({
-  plugins: ["remove_button"],
-  sortField: "text"
-})
+$('#select-age').selectize({
+  plugins: ['remove_button'],
+  sortField: 'text'
+});
+
+const toggleBurger = (e) => {
+  e.classList.toggle('change');
+  filtersList.classList.toggle('open');
+};
