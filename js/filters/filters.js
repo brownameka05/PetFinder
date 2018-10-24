2d8354a8d5e027116549bae0ebf6bf48a98c8c5a
ttlekich <ttlekich@gmail.com>
2018-10-24 11:52:30 -0500

    filtered dogs

--- js/filters/filters.js
+++ js/filters/filters.js
@@ -219,26 +219,17 @@ $("#textbox-location").selectize({
  *  State
  */
 let petFilters = {
-  key: "",
-  animals: [],
-  breeds: [],
-  sizes: [], // S, M, L or XL
-  sexes: [], // M or F
-  location: "", // zipcode
-  ages: [], // Baby, Young, Adult, Senior
-  offset: "", // set this to the value of lastOffset returned by a previous call to pet.find, and it will retrieve the next result set
-  count: 25,
-  output: "full", // or basic
-  format: "json"
+  animal: [],
+  // breeds: [],
+  size: [], // S, M, L or XL
+  sex: [], // M or F
+  age: ["Baby", "Young"] // Baby, Young, Adult, Senior
 }
 
 const shelterFilters = {
-  key: "",
   location: "", // zipCode
   name: "", // shelter name
-  offset: "",
-  count: 25,
-  format: "json"
+  offset: ""
 }
 
 const searchViewState = {
--- js/script.js
+++ js/script.js
@@ -1,49 +1,63 @@
-function populateSearchResults(resultsObject){
-    for(key in resultsObject){
-        petObject = resultsObject[key]
-        petLiteral = `
+const filterPets = (currentPets, filter) => {
+  if (areFiltersEmpty(filter)) {
+    return currentPets
+  }
+  return Object.keys(currentPets)
+    .map(function(key) {
+      return currentPets[key]
+    })
+    .filter(pet => petMatch(filter, pet))
+}
+
+const areFiltersEmpty = obj => {
+  let isEmpty = true
+  for (k in obj) {
+    if (obj[k].length !== 0) {
+      isEmpty = false
+    }
+  }
+  return isEmpty
+}
+
+const isFilterEmpty = filterCategory => {
+  return filterCategory.length === 0
+}
+
+// filter, pet -> bool
+const petMatch = (filters, pet) => {
+  for (filterKey in filters) {
+    if (!isFilterEmpty(filters[filterKey])) {
+      if (!filters[filterKey].includes(pet[filterKey])) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+function populateSearchResults(currentPets, filter) {
+  const pets = filterPets(currentPets, filter)
+  console.log(pets)
+  Object.keys(pets)
+    .map(key => currentPets[key])
+    .slice(0, 25)
+    .map(pet => {
+      petLiteral = `
         <div class="card">
-            <img class="card-img" src="https://lh5.googleusercontent.com/-2cuebuSKiRU/AAAAAAAAAAI/AAAAAAAAAEU/PibNivK-4U4/photo.jpg" alt="Card image cap">
+            <img class="card-imkg" src="https://lh5.googleusercontent.com/-2cuebuSKiRU/AAAAAAAAAAI/AAAAAAAAAEU/PibNivK-4U4/photo.jpg" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">${petObject.name}</h5>
-                <p class="card-text">Age: ${petObject.age}</p>
-                <button onclick = class = "learnMore" id = "${key}">More about ${petObject.name}</button>
+                <h5 class="card-title">${pet.name}</h5>
+                <p class="card-text">Age: ${pet.age}</p>
+                <button onclick = class = "learnMore" id = "${
+                  pet.id
+                }">More about ${pet.name}</button>
             </div>
         </div>
         `
-        $("#results").append(petLiteral)
-    }
+      $("#results").append(petLiteral)
+    })
 }
 
-petObject = {
-    chico : {
-        name: "Chico",
-        age: 12
-    }, 
-    bruce : {
-        name: "Bruce",
-        age: 2
-    },
-    bailey : {
-        name: "Bailey",
-        age: 12
-    }, 
-    lucy : {
-        name: "Lucy",
-        age: 2
-    }, 
-    charlie : {
-        name: "Charlie",
-        age: 2
-    },
-    molly : {
-        name: "Molly",
-        age: 12
-    }, 
-    max : {
-        name: "Max",
-        age: 2
-    }
-}
-
-populateSearchResults(petObject)
+initPage().then(results =>
+  populateSearchResults(results.currentPets, petFilters)
+)
