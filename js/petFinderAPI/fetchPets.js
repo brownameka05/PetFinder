3838f39bd2592ee7b8e86e6d13fbd814998f7272
Travis Lekich <ttlekich@gmail.com>
2018-10-23 20:57:51 -0500

    apparently we need to use jquery with the petfinder api...who knew

--- js/petFinderAPI/fetchPets.js
+++ js/petFinderAPI/fetchPets.js
@@ -1,27 +1,30 @@
 // ! key and secret are provided from"secrets.js" not included in version control.
 // "http://api.petfinder.com/my.method?key=12345&arg1=foo"
 
-const BASE_URL = "http://api.petfinder.com/"
+const BASE_URL = 'http://api.petfinder.com/';
 const METHODS = {
-  breedList: "breed.list", // returns a list of breeds for a particular animal
-  getPet: "pet.get", // returns a pet
-  getPets: "pet.find", // returns a collection of pets
-  getShelters: "shelter.find", // returns a collection of shelters
-  getShelter: "shelter.get" // returns a shelter
-}
+  breedList: 'breed.list', // returns a list of breeds for a particular animal
+  getPet: 'pet.get', // returns a pet
+  getPets: 'pet.find', // returns a collection of pets
+  getShelters: 'shelter.find', // returns a collection of shelters
+  getShelter: 'shelter.get' // returns a shelter
+};
+const FETCH_CONFIG = {};
 
 // filters -> query string
 const createQuery = (method, key, args) => {
-  return `${BASE_URL}${method}?format=json&key=${key}&${args}`
-}
+  return `${BASE_URL}${method}?format=json&key=${key}&${args}&callback=?`;
+};
 
 // animal ("dog" or "cat") -> Promise(breeds list)
-const getAllBreedsOf = async animal => {
-  const query = createQuery(METHODS.breedList, key, `animal=${animal}`)
-  return fetch(query)
-    .then(res => res.json())
-    .then(json => json.petfinder.breeds.breed.map(obj => Object.values(obj)[0]))
-}
+const getAllBreedsOf = async (animal) => {
+  const query = createQuery(METHODS.breedList, key, `animal=${animal}`);
+  return $.getJSON(query)
+    .then((json) =>
+      json.petfinder.breeds.breed.map((obj) => Object.values(obj)[0])
+    )
+    .catch((err) => console.log(err));
+};
 
 // location, offset, count -> json/object
 const getPetsAtLocation = (location, offset = 0, count = 100) => {
@@ -29,13 +32,13 @@ const getPetsAtLocation = (location, offset = 0, count = 100) => {
     METHODS.getPets,
     key,
     `location=${location}&offset=${offset}&count=${count}`
-  )
-  return fetch(query)
-    .then(res => res.json())
-    .then(json => {
-      return [json.petfinder.pets.pet, offset + count]
+  );
+  return $.getJSON(query)
+    .then((json) => {
+      return [json.petfinder.pets.pet, offset + count];
     })
-}
+    .catch((err) => console.log(err));
+};
 
 // location, offset, count -> json/object
 const getSheltersAtLocation = (location, offset = 0, count = 25) => {
@@ -43,30 +46,36 @@ const getSheltersAtLocation = (location, offset = 0, count = 25) => {
     METHODS.getShelters,
     key,
     `location=${location}&offset=${offset}&count=${count}`
-  )
-  return fetch(query)
-    .then(res => res.json())
-    .then(json => json.petfinder.shelters.shelter)
-    .then(sheltersList =>
+  );
+  return $.getJSON(query)
+    .then((json) => json.petfinder.shelters.shelter)
+    .then((sheltersList) =>
       sheltersList.map(flattenShelterObj).map(convertLonLat)
-    )
-}
+    );
+};
 
 // Helpers
-const flattenShelterObj = shelter => {
-  const shelterKeys = Object.keys(shelter)
+const flattenShelterObj = (shelter) => {
+  const shelterKeys = Object.keys(shelter);
   return shelterKeys.reduce((obj, key) => {
-    obj[key] = shelter[key] ? Object.values(shelter[key])[0] : ""
-    return obj
-  }, {})
-}
+    obj[key] = shelter[key] ? Object.values(shelter[key])[0] : '';
+    return obj;
+  }, {});
+};
 
-const convertLonLat = shelterObj => {
-  shelterObj.longitude = parseFloat(shelterObj.longitude)
-  shelterObj.latitude = parseFloat(shelterObj.latitude)
-  return shelterObj
-}
+const convertLonLat = (shelterObj) => {
+  shelterObj.longitude = parseFloat(shelterObj.longitude);
+  shelterObj.latitude = parseFloat(shelterObj.latitude);
+  return shelterObj;
+};
 
 // Tests
-getSheltersAtLocation("77025").then(data => console.log(data))
-console.log("hello")
+getSheltersAtLocation('77025').then((data) => {
+  console.log('success');
+  console.log(data);
+});
+
+getPetsAtLocation('77025').then((data) => {
+  console.log('success');
+  console.log(data);
+});
