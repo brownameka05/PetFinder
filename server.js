526936da66ddfcd8d5a34f1abb0d4fe968bdffef
Travis Lekich <ttlekich@gmail.com>
2018-10-24 20:10:16 -0500

    server init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+  app.use(express.static('public'));
+});
+
+app.listen(port, () => console.log(`Example app listening on prot ${port}!`));
