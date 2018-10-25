2dc7c3bec75cb56a24376ec41afdc858e6a63dff
Travis Lekich <ttlekich@gmail.com>
2018-10-25 17:57:25 -0500

    post merge fixes

--- js/map.js
+++ js/map.js
@@ -1,86 +1,76 @@
-var map
+var map;
 
 function initMap() {
-  map = new google.maps.Map($("#map")[0], {
+  map = new google.maps.Map($('#map')[0], {
     center: { lat: 39.81, lng: -98.5556 },
     zoom: 4
-  })
+  });
   //Try HTML5 geolocation
-  let location = "77025"
-  let geocoder = new google.maps.Geocoder()
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function(position) {
-        // var pos = {
-        //   lat: position.coords.latitude,
-        //   lng: position.coords.longitude
-        // }
-        // geocoder
-        //   .geocode({ location: pos }, res => {
-        //     g(res[0])
-        //   })
-        //   .then()
-        // console.log(location)
-        // console.log("prestart")
-        // startUp()
-        // map.setCenter(pos)
-        // map.setZoom(12)
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+        map.setZoom(12);
       },
       function() {
-        handleLocationError(true, infoWindow, map.getCenter())
+        handleLocationError(true, infoWindow, map.getCenter());
       }
-    )
+    );
   } else {
     // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter())
+    handleLocationError(false, infoWindow, map.getCenter());
   }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos)
+  infoWindow.setPosition(pos);
   infoWindow.setContent(
     browserHasGeolocation
-      ? "Error: The Geolocation service failed."
+      ? 'Error: The Geolocation service failed.'
       : "Error: Your browser doesn't support geolocation."
-  )
-  infoWindow.open(map)
+  );
+  infoWindow.open(map);
 }
 
 function setSheltersOnMap(shelterArray) {
   Object.keys(shelterArray)
-    .map(key => shelterArray[key])
-    .map(shelter => {
-      let latitude = shelter.latitude
-      let longitude = shelter.longitude
-      let location = new google.maps.LatLng(latitude, longitude)
+    .map((key) => shelterArray[key])
+    .map((shelter) => {
+      let latitude = shelter.latitude;
+      let longitude = shelter.longitude;
+      let location = new google.maps.LatLng(latitude, longitude);
       let contentString = `
         <b>${shelter.name}</b>
         <p>${shelter.email}</p>
-        <p>${shelter.phone ? shelter.phone : ""}</p>
-        `
+        <p>${shelter.phone ? shelter.phone : ''}</p>
+        `;
       let infowindow = new google.maps.InfoWindow({
         content: contentString
-      })
+      });
       let marker = new google.maps.Marker({
         position: location,
         map: map,
         title: shelter.name,
         name: shelter.name
-      })
-      marker.addListener("mouseover", function() {
-        infowindow.open(map, marker)
-        marker.addListener("mouseout", function() {
-          infowindow.close()
-        })
-      })
-      marker.addListener("mouseout", function() {
-        infowindow.close()
-      })
-      marker.addListener("click", function() {
-        infowindow.open(map, marker)
-        google.maps.event.clearListeners(marker, "mouseout")
-      })
-    })
+      });
+      marker.addListener('mouseover', function() {
+        infowindow.open(map, marker);
+        marker.addListener('mouseout', function() {
+          infowindow.close();
+        });
+      });
+      marker.addListener('mouseout', function() {
+        infowindow.close();
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+        google.maps.event.clearListeners(marker, 'mouseout');
+      });
+    });
 }
 
 // address1: "8620 Stella Link Road"
--- js/script.js
+++ js/script.js
@@ -1,60 +1,60 @@
 const filterPets = (currentPets, filter) => {
   if (areFiltersEmpty(filter)) {
-    return currentPets
+    return currentPets;
   }
   return Object.keys(currentPets)
     .map(function(key) {
-      return currentPets[key]
+      return currentPets[key];
     })
-    .filter(pet => petMatch(filter, pet))
-}
+    .filter((pet) => petMatch(filter, pet));
+};
 
-const areFiltersEmpty = obj => {
-  let isEmpty = true
+const areFiltersEmpty = (obj) => {
+  let isEmpty = true;
   for (k in obj) {
     if (obj[k].length !== 0) {
-      isEmpty = false
+      isEmpty = false;
     }
   }
-  return isEmpty
-}
+  return isEmpty;
+};
 
-const isFilterEmpty = filterCategory => {
-  return filterCategory.length === 0
-}
+const isFilterEmpty = (filterCategory) => {
+  return filterCategory.length === 0;
+};
 
 // filter, pet -> bool
 const petMatch = (filters, pet) => {
   for (filterKey in filters) {
     if (!isFilterEmpty(filters[filterKey])) {
       if (!fineOne(filters[filterKey], pet[filterKey])) {
-        return false
+        return false;
       }
     }
   }
-  return true
-}
+  return true;
+};
 
 const fineOne = (haystack, arr) => {
-  if (typeof arr === "string") {
-    arr = [arr]
+  if (typeof arr === 'string') {
+    arr = [arr];
   }
-  return arr.some(v => {
-    return haystack.indexOf(v) >= 0
-  })
-}
+  return arr.some((v) => {
+    return haystack.indexOf(v) >= 0;
+  });
+};
 
 function populateSearchResults(currentPets, filter, from, to) {
-  const pets = filterPets(currentPets, filter)
+  const pets = filterPets(currentPets, filter);
   const petHtml = Object.keys(pets)
-    .map(key => pets[key])
+    .map((key) => pets[key])
 
     .slice(from, to)
-    .map(pet => {
+    .map((pet) => {
       petLiteral = `
         <div class="card">
             <img class="card-img" src="${
-              pet.imgUrls ? pet.imgUrls[3] : "#"
+              pet.imgUrls ? pet.imgUrls[3] : '#'
             }" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title">${pet.name}</h5>
@@ -64,67 +64,65 @@ function populateSearchResults(currentPets, filter, from, to) {
         pet.name
       }</button>
             </div>
-        </div> `
-      return petLiteral
-    })
-  $("#results").html(
-    petHtml.length > 0 ? petHtml.reduce((a, b) => a + b) : "No matching pets :("
-  )
+        </div> `;
+      return petLiteral;
+    });
+  $('#results').html(
+    petHtml.length > 0 ? petHtml.reduce((a, b) => a + b) : 'No matching pets :('
+  );
 }
 
 const setBackButtonCSS = () => {
   if (offSetState.from === 0) {
-    $("#btn-back").css("background", "lightgray")
-    $("#btn-back:active").css("background", "lightgray")
+    $('#btn-back').css('background', 'lightgray');
+    $('#btn-back:active').css('background', 'lightgray');
   } else {
-    $("#btn-back").css("background", "#343a40")
+    $('#btn-back').css('background', '#343a40');
   }
-}
+};
 
-const startUp = async () => {
-  initPage()
-    .then(petData => {
+initPage()
+  .then((petData) => {
+    populateSearchResults(
+      petData.currentPets,
+      petFilters,
+      offSetState.from,
+      offSetState.to
+    );
+    setSheltersOnMap(petData.shelters);
+
+    $('#btn-apply').click((e) => {
+      $('#results').html('');
+      populateSearchResults(petData.currentPets, petFilters);
+      initializeBreedFilter(petData, petFilters);
+      console.log(shelterFilters);
+    });
+
+    $('#btn-next').click((e) => {
+      $('#results').html('');
+      offSetState = setOffSet(offSetState, 24);
+      setBackButtonCSS();
       populateSearchResults(
         petData.currentPets,
         petFilters,
         offSetState.from,
         offSetState.to
-      )
-      setSheltersOnMap(petData.shelters)
+      );
+    });
 
-      $("#btn-apply").click(e => {
-        $("#results").html("")
-        populateSearchResults(petData.currentPets, petFilters)
-        initializeBreedFilter(petData, petFilters)
-        console.log(shelterFilters)
-      })
+    $('#btn-back').click((e) => {
+      $('#results').html('');
+      offSetState = setOffSet(offSetState, 24, 'back');
+      setBackButtonCSS();
+      populateSearchResults(
+        petData.currentPets,
+        petFilters,
+        offSetState.from,
+        offSetState.to
+      );
+    });
 
-      $("#btn-next").click(e => {
-        $("#results").html("")
-        offSetState = setOffSet(offSetState, 24)
-        setBackButtonCSS()
-        populateSearchResults(
-          petData.currentPets,
-          petFilters,
-          offSetState.from,
-          offSetState.to
-        )
-      })
-
-      $("#btn-back").click(e => {
-        $("#results").html("")
-        offSetState = setOffSet(offSetState, 24, "back")
-        setBackButtonCSS()
-        populateSearchResults(
-          petData.currentPets,
-          petFilters,
-          offSetState.from,
-          offSetState.to
-        )
-      })
-
-      initializeBreedFilter(petData, petFilters)
-      initializeTypeFilter(petData)
-    })
-    .catch(err => console.log(err))
-}
+    initializeBreedFilter(petData, petFilters);
+    initializeTypeFilter(petData);
+  })
+  .catch((err) => console.log(err));
