5d06be8bda71b13fd63af34e8aaf3d4e4ebc5cd0
ttlekich <ttlekich@gmail.com>
2018-10-26 08:38:01 -0500

    initial location

--- js/app.js
+++ js/app.js
@@ -1,77 +1,73 @@
 // Initialize Page Data
-const initPage = async (loc) => {
-  let petData = {};
-  petData.dogBreeds = await getAllBreedsOf('dog');
-  petData.catBreeds = await getAllBreedsOf('cat');
-  petData.currentPets = await getPetsAtLocation(loc);
-  petData.shelters = await getSheltersAtLocation(loc);
-  return petData;
-};
+const getData = async loc => {
+  let petData = {}
+  petData.dogBreeds = await getAllBreedsOf("dog")
+  petData.catBreeds = await getAllBreedsOf("cat")
+  petData.currentPets = await getPetsAtLocation(loc)
+  petData.shelters = await getSheltersAtLocation(loc)
+  return petData
+}
 
-const doEverything = (loc) => {
-  console.log(loc);
-  initPage(loc)
-    .then((petData) => {
+const search = loc => {
+  getData(loc).then(petData => {
+    populateSearchResults(
+      petData.currentPets,
+      petFilters,
+      offSetState.from,
+      offSetState.to
+    )
+    setSheltersOnMap(petData.shelters)
+    initializeBreedFilter(petData, petFilters)
+    initializeTypeFilter(petData)
+    getIniticalLocation()
+
+    $("#btn-apply").click(e => {
+      $("#results").html("")
       populateSearchResults(
         petData.currentPets,
         petFilters,
         offSetState.from,
         offSetState.to
-      );
-      setSheltersOnMap(petData.shelters);
-      initializeBreedFilter(petData, petFilters);
-      initializeTypeFilter(petData);
-
-      $('#btn-apply').click((e) => {
-        $('#results').html('');
-        populateSearchResults(
-          petData.currentPets,
-          petFilters,
-          offSetState.from,
-          offSetState.to
-        );
-        initializeBreedFilter(petData, petFilters);
-        updateMapFromZip(shelterFilters.location);
-      });
-
-      $('#btn-next').click((e) => {
-        $('#results').html('');
-        offSetState = setOffSet(offSetState, 24);
-        setBackButtonCSS();
-        populateSearchResults(
-          petData.currentPets,
-          petFilters,
-          offSetState.from,
-          offSetState.to
-        );
-      });
-
-      $('#btn-back').click((e) => {
-        $('#results').html('');
-        offSetState = setOffSet(offSetState, 24, 'back');
-        setBackButtonCSS();
-        populateSearchResults(
-          petData.currentPets,
-          petFilters,
-          offSetState.from,
-          offSetState.to
-        );
-      });
+      )
+      initializeBreedFilter(petData, petFilters)
+      updateMapFromZip(shelterFilters.location)
     })
-    .catch((err) => console.log(err));
-};
 
-$('#select-location-form').submit((e) => {
-  e.preventDefault();
+    $("#btn-next").click(e => {
+      $("#results").html("")
+      offSetState = setOffSet(offSetState, 24)
+      setBackButtonCSS()
+      populateSearchResults(
+        petData.currentPets,
+        petFilters,
+        offSetState.from,
+        offSetState.to
+      )
+    })
+
+    $("#btn-back").click(e => {
+      $("#results").html("")
+      offSetState = setOffSet(offSetState, 24, "back")
+      setBackButtonCSS()
+      populateSearchResults(
+        petData.currentPets,
+        petFilters,
+        offSetState.from,
+        offSetState.to
+      )
+    })
+  })
+}
+
+$("#select-location-form").submit(e => {
+  e.preventDefault()
   shelterFilters = setFilters(shelterFilters, {
     location: e.currentTarget[0].value
-  });
+  })
   shelterFilters = setFilters(shelterFilters, {
     from: 0,
     to: 24
-  });
-  updateMapFromZip(shelterFilters.location);
-  doEverything(shelterFilters.location);
-});
-
-doEverything('77025');
+  })
+  updateMapFromZip(shelterFilters.location)
+  search(shelterFilters.location)
+})
