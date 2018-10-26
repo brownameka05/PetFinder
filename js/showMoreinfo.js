4309bb281ff0f6b3e9586769e18559fe5f22cb3f
Ameka Brown <brownameka05@gmail.com>
2018-10-26 14:10:21 -0500

    changes

--- js/showMoreinfo.js
+++ js/showMoreinfo.js
@@ -1,9 +1,8 @@
 
-<<<<<<< HEAD
 
 function closemyfunction(infoPet){
   infoPet.parentElement.parentElement.removeChild(infoPet.parentElement)
-
+  console.log(infoPet)
 }
 
 
@@ -12,16 +11,15 @@ function closemyfunction(infoPet){
 
 
 
-
-=======
->>>>>>> d64529b94b55b289528c3ad2b3a2d4f6f5bc3b1d
-function showmoreinfo(petID){
+function showMoreInfo(petID){
   getPet(petID).then(function(pet){
+    console.log(pet)
     return thisPet = flattenPet(pet)
   }).then(function(newPet){
     petLiteral = `
     <div id = "learnMore">
-      <img src = \"${newPet.images[0]} />
+      <button onclick="closemyfunction(this)" id="btnInfo" > X </button>
+      <img src = \"${newPet.images[0]}\" />
       <h3>Name: ${newPet.name}</h3>
       <p>Breed: ${newPet.breed}</p>
       <p>Sex: ${newPet.sex}</p>
@@ -35,27 +33,16 @@ function showmoreinfo(petID){
       petLiteral += `     <p>Phone: ${newPet.shelterInfo.phone}</p>`
     }
 
-<<<<<<< HEAD
-    infoLiteral = `
-    <div id="learnMore">
-      <button onclick="closemyfunction(this)" id="btnInfo"> X </Button>
-      <img id="petPic" src="https://media.mwstatic.com/product-images/880x660/alt1/962/962441.jpg">
-      <h3>Rex</h3>
-      <p>Age: 3</p>
-      <p>Breed: Boxer Mix</p>
-      <p>Good With: kids, cats, babies</p>
-      <p>About me:  I love to play in water and cuddle at night. I don't bark much but I love to eat when you do. I don't like water but if you give me a loving bath I wont be too much trouble</p>
-    <div>
-=======
+
     if(!jQuery.isEmptyObject(newPet.shelterInfo.email)){
       petLiteral += `      <p>Email: ${newPet.shelterInfo.email}</p>`
     }
     petLiteral +=`
       </div>
     </div>
->>>>>>> d64529b94b55b289528c3ad2b3a2d4f6f5bc3b1d
     `
-    
+    $("#section1").append(petLiteral)
+
   })
 
 }
@@ -88,7 +75,7 @@ function flattenShelterResponse(apiResponse){
     newShelter['email'] = shelter.email.$t
   }
   if(!jQuery.isEmptyObject(shelter.latitude) && !jQuery.isEmptyObject(shelter.longitude)){
-    newShelter['geoLocation'] = {'latitude' : shelter.latitude.$t, 'longitude' : shelter.longitude.$t} 
+    newShelter['geoLocation'] = {'latitude' : shelter.latitude.$t, 'longitude' : shelter.longitude.$t}
   }
   return newShelter
 }
