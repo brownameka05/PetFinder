126565894f241ee86d0f610209e54631e5340551
ttlekich <ttlekich@gmail.com>
2018-10-26 14:49:49 -0500

    custom end of results

---
+++ js/errorMsgs/errorNoMoreResults.js
@@ -0,0 +1,40 @@
+const noMoreResultsHTML = /*html*/ `
+<div id="errorDogContainer">
+<div id="end-results-msg">Sorry...no more pets to display!</div>
+<div class="dog">
+  <div class="dog-body">
+    <div class="dog-tail">
+      <div class="dog-tail">
+        <div class="dog-tail">
+          <div class="dog-tail">
+            <div class="dog-tail">
+              <div class="dog-tail">
+                <div class="dog-tail">
+                  <div class="dog-tail"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="dog-torso"></div>
+  <div class="dog-head">
+    <div class="dog-ears">
+      <div class="dog-ear"></div>
+      <div class="dog-ear"></div>
+    </div>
+    <div class="dog-eyes">
+      <div class="dog-eye"></div>
+      <div class="dog-eye"></div>
+    </div>
+    <div class="dog-muzzle">
+      <div class="dog-tongue"></div>
+    </div>
+  </div>
+</div>
+
+<div class="error-ball" tabindex="0"></div>
+</div>
+`
--- js/petFinderAPI/queryState.js
+++ js/petFinderAPI/queryState.js
@@ -1,29 +1,36 @@
 let offSetState = {
   from: 0,
   to: 24
-};
+}
+
+const resetOffSetState = () => {
+  return {
+    from: 0,
+    to: 24
+  }
+}
 
 let queryParams = {
-  nPets: 250,
+  nPets: 40,
   nShelters: 50,
   offsetPets: 0,
   offsetShelters: 0
-};
+}
 
-let userLocation = '';
+let userLocation = ""
 
-const setOffSet = (offSetState, n, dir = 'forward') => {
-  if (dir === 'forward') {
+const setOffSet = (offSetState, n, dir = "forward") => {
+  if (dir === "forward") {
     return {
       from: offSetState.to,
       to: offSetState.to + n
-    };
-  } else if (dir === 'back' && offSetState.from !== 0) {
+    }
+  } else if (dir === "back" && offSetState.from !== 0) {
     return {
       from: offSetState.from - n,
       to: offSetState.to - n
-    };
+    }
   } else {
-    return offSetState;
+    return offSetState
   }
-};
+}
